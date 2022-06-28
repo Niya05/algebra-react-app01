f5d0e741859a381b3f11b57db29924e38b826091
Niya <disma1589@gmail.com>
2022-06-28 19:27:46 +0200

    added user image

--- src/App.js
+++ src/App.js
@@ -3,29 +3,34 @@ import { InputElement } from "./components/InputElement";
 import Button from "./components/button";
 import FormError from "./components/FormError";
 import FormMessage from "./components/FormMessage";
+import { User } from "./components/User";
 
 // Komponenta - gradivna jedinica Reacta
 
 function App() {
+  /* parent - putem props (label, type) komunicira s child komponentom */
   const isSignedIn = true;
 
   return (
-    <form className="form">
-      <div className="form-field">
-        <InputElement label="Username" type="text" />
-      </div>
-      <div className="form-field">
-        <InputElement label="Password" type="password" />
-      </div>
-      <div className="form-field">
-        <Button type="button">Sign in</Button>
-        <Button type="reset">Reset</Button>
-      </div>
-      <div className="message-field">
-        <FormError visible={!isSignedIn} />
-        <FormMessage visible={isSignedIn} />
-      </div>
-    </form>
+    <div>
+      <User src="/owlbear.png" username="Niya" />
+      <form className="form">
+        <div className="form-field">
+          <InputElement label="Username" type="text" />
+        </div>
+        <div className="form-field">
+          <InputElement label="Password" type="password" />
+        </div>
+        <div className="form-field">
+          <Button type="button">Sign in</Button>
+          <Button type="reset">Reset</Button>
+        </div>
+        <div className="message-field">
+          <FormError visible={!isSignedIn} />
+          <FormMessage visible={isSignedIn} />
+        </div>
+      </form>
+    </div>
   );
 }
 
