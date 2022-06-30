9d51efc3425b3ed79f31758feefe6e67047f8f73
Niya <disma1589@gmail.com>
2022-06-30 19:02:33 +0200

    added sign in and chat pages

--- src/App.js
+++ src/App.js
@@ -1,69 +1,25 @@
 import "./App.css";
-import { InputElement } from "./components/InputElement";
-import Button from "./components/Button";
-import FormError from "./components/FormError";
-import FormMessage from "./components/FormMessage";
-import { User } from "./components/User";
 import { useState } from "react";
+import { SignInPage } from "./pages/SignInPage";
+import { ChatRoomPage } from "./pages/ChatRoomPage";
 
 function App() {
-  const [formState, setFormState] = useState({ username: "" });
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
-
-    if (formState.username === "") {
-      setError(true);
-    } else {
-      setUser(formState);
-      setError(null);
-      setFormState({ username: "" });
-    }
+  const storeUser = (user) => {
+    setUser(user);
   };
 
-  const handleChange = (event) => {
-    setFormState((state) => ({
-      ...state,
-      [event.target.name]: event.target.value,
-    }));
-  };
-
-  const signOut = () => {
+  const clearUser = () => {
     setUser(null);
-    setError(null);
   };
 
   const isSignedIn = user !== null;
-  const showErrorMessage = error !== null;
-  const showFormMessage = user !== null;
 
   return (
     <div>
-      {isSignedIn && (
-        <Button type="button" onClick={signOut}>
-          Sign out
-        </Button>
-      )}
-      {isSignedIn && <User src="/owlbear.png" username={user.username} />}
-      {!isSignedIn && (
-        <form className="form" onSubmit={handleSubmit}>
-          <div className="form-field">
-            <InputElement
-              name="username"
-              label="Username"
-              type="text"
-              onChange={handleChange}
-            />
-          </div>
-          <div className="form-field">
-            <Button type="submit">Sign in</Button>
-          </div>
-          <FormError visible={showErrorMessage} />
-          <FormMessage visible={showFormMessage} />
-        </form>
-      )}
+      {isSignedIn && <ChatRoomPage onSignOut={clearUser} user={user} />}
+      {!isSignedIn && <SignInPage onSignIn={storeUser} />}
     </div>
   );
 }
